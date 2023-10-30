c91dea5e6e0b54c85dd8128ba6c8c89fa846bc93
Kyoto430 <nateriver345@gmail.com>
2023-10-30 16:56:31 +0300

    feat: add adaptive

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,16 +2,24 @@ console.log('slider running...');
 
 new Swiper('.staff__slider', {
   loop: false,
+  pagination: {
+    el: '.s__p--1',
+    clickable: true,
+  },
   navigation: {
     prevEl: '.slider__btn-prev--1',
     nextEl: '.slider__btn-next--1',
   },
+  uniqueNavElements: false,
   breakpoints: {
     1920: {
       slidesPerView: 6,
     },
     1280: {
       slidesPerView: 6,
+    },
+    1230: {
+      slidesPerView: 2,
     },
     768: {
       slidesPerView: 2,
@@ -26,16 +34,24 @@ new Swiper('.staff__slider', {
 
 new Swiper('.staff-clients__slider', {
   loop: false,
+  pagination: {
+    el: '.s__p--2',
+    clickable: true,
+  },
   navigation: {
     prevEl: '.slider__btn-prev--2',
     nextEl: '.slider__btn-next--2',
   },
+  uniqueNavElements: false,
   breakpoints: {
     1920: {
       slidesPerView: 4,
     },
     1280: {
       slidesPerView: 4,
+    },
+    1230: {
+      slidesPerView: 3,
     },
     768: {
       slidesPerView: 2,
@@ -51,19 +67,23 @@ new Swiper('.staff-clients__slider', {
 new Swiper('.clients__slider', {
   loop: true,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.s__p--3',
     clickable: true,
   },
   navigation: {
     prevEl: '.slider__btn-prev--3',
     nextEl: '.slider__btn-next--3',
   },
+  uniqueNavElements: false,
   breakpoints: {
     1920: {
       slidesPerView: 4,
     },
     1280: {
       slidesPerView: 4,
+    },
+    1230: {
+      slidesPerView: 3,
     },
     768: {
       slidesPerView: 2,
